15da16d4685a895d1e445b4350b507caa7ee01b2
hyundonglee <haksa1008@gmail.com>
2024-12-06 12:20:53 +0900

    initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
+
+// 아래 코드를 삭제하거나 주석 처리합니다
+// import reportWebVitals from './reportWebVitals';
+// reportWebVitals();
